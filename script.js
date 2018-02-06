23c566cfc35e4334fb23d841cada24ad9011ef9c
Louis Kraemer <louiskraemer@gmail.com>
2018-02-06 10:51:27 +0100

    Front is now responsive

--- script.js
+++ script.js
@@ -1,53 +1,115 @@
 d3.json("data/json/map.json", function (json) {
-    console.log(json)
-
-    var margin = {
-            top: '20px',
-            bottom: '20px',
-            left: '20px',
-            right: '20px'
-        },
-//        width = windows,
-//        height = 580,
-        scale = (width - 1) / Math.PI,
-        svg = d3.select("body")
+    var svg = d3.select("#map-container")
         .append("svg")
-        .attr('class', 'map-container');
-    //            .attr("width", width)
-    //            .attr("height", height);
-    //
-    //    var tooltip = d3.select('body').append('div')
-    //        .attr('class', 'hidden tooltip');
-    //
-
-
-
-    var center = d3.geoCentroid(json);
-
-    console.log((center))
-    //
-    var projection = d3.geoConicConformal()
+        .attr('id', 'map')
+        .attr('width', '100%'),
+        initScale = 345000,
+        center = d3.geoCentroid(json),
+        width = parseInt(d3.select('#map').style('width')),
+        projection = d3.geoConicConformal()
         //        .translate([width / 2, height / 2])
         .center(center)
-        .scale(2000);
+        .scale(initScale),
+        path = d3.geoPath()
+        .projection(projection),
+        bounds = path.bounds(json),
+        ratio = (bounds[1][0] - bounds[0][0]) / (bounds[1][1] - bounds[0][1]),
+        height = width / ratio;
 
-    var path = d3.geoPath()
-        .projection(projection);
+    d3.select("#map").attr('height', height);
 
-    var bounds = path.bounds(json);
-    var hscale = scale * width / (bounds[1][0] - bounds[0][0]);
-    var vscale = scale * height / (bounds[1][1] - bounds[0][1]);
-    var scale = (hscale < vscale) ? hscale : vscale;
-    var offset = [width - (bounds[0][0] + bounds[1][0]) / 2,
-                                height - (bounds[0][1] + bounds[1][1]) / 2];
-
-    projection.scale(scale).translate(offset);
+    projection.translate([width / 2, height / 2]);
 
     svg.selectAll("path")
         .data(json.features)
         .enter()
         .append("path")
         .attr("d", path);
+
+    resize();
+
+    d3.select(window).on('resize', resize);
+
+    function resize() {
+        if (parseInt(d3.select('#map').style('width')) !== width) {
+            width = parseInt(d3.select('#map').style('width'));
+            height = width / ratio;
+            d3.select("#map").attr('height', height);
+
+            var scale = (width / (bounds[1][0] - bounds[0][0])) * initScale;
+
+            projection
+                .translate([width / 2, height / 2])
+                .scale(scale);
+
+            d3.selectAll("path").attr("d", path);
+
+        }
+    }
+
+    //        width = parseInt(d3.select('#map').style('width')),
+    //        width = width - margin.left - margin.right,
+    //        mapRatio = .5,
+    //        height = width * mapRatio;
+
+    //    var height = 0,
+    //        width = 0,
+    //        center = d3.geoCentroid(json),
+    //        margin = {
+    //            top: '20px',
+    //            bottom: '20px',
+    //            left: '20px',
+    //            right: '20px'
+    //        },
+    //        svg = d3.select("body")
+    //        .append("svg")
+    //        .attr('id', 'map-container'),
+    //        projection = d3.geoConicConformal()
+    //        .translate([width / 2, height / 2])
+    //        .center(center)
+    //        .scale(200000),
+    //        path = d3.geoPath()
+    //        .projection(projection),
+    //        bounds = path.bounds(json);
+
+    //    const resize = function () {
+    //        console.log('resizing')
+    //        var scale = 100;
+    //        var height = body.clientHeight;
+    //        var width = body.clientWidth;
+    //        //        const hscale = scale * width / (bounds[1][0] - bounds[0][0]);
+    //        //        const vscale = scale * height / (bounds[1][1] - bounds[0][1]);
+    //        //        scale = (hscale < vscale) ? hscale : vscale;
+    //        //        const offset = [width - (bounds[0][0] + bounds[1][0]) / 2,
+    //        //                                height - (bounds[0][1] + bounds[1][1]) / 2];
+    //
+    //        var bounds = path.bounds(json);
+    //        var hscale = scale * width / (bounds[1][0] - bounds[0][0]);
+    //        var vscale = scale * height / (bounds[1][1] - bounds[0][1]);
+    //        var scale = (hscale < vscale) ? hscale : vscale;
+    //        var offset = [width - (bounds[0][0] + bounds[1][0]) / 2,
+    //                        height - (bounds[0][1] + bounds[1][1]) / 2];
+    //
+    //        console.log(scale)
+    //
+    //        projection = d3.geoConicConformal().center(center)
+    //            .scale(scale)
+    //        //            .translate(offset);
+    //        path = d3.geoPath()
+    //            .projection(projection)
+    //        //        projection.scale(scale).translate(offset)
+    //    }
+
+    //    resize();
+    //
+    //    window.onresize = resize;
+
+
+
+    //
+    //    projection.scale(scale).translate(offset);
+
+    //    
 
     //    d3.csv("grippe.csv", function (data) {
     //        d3.json("map.json", function (json) {
