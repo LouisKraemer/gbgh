25ec94aaba988ba88fd098e4677502715cb796cc
gcreti <gabrielcreti@gmail.com>
2018-02-20 17:19:37 +0100

    modifications nettoyage data

--- backend/getVelov.py
+++ backend/getVelov.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 from pymongo import MongoClient
+from pymongo import GEOSPHERE
+from bson.objectid import ObjectId
 import requests
 import datetime
 import time
@@ -12,25 +14,67 @@ logging.basicConfig(level=logging.INFO,
                     filename='/tmp/getVelovs.log',
                     filemode='a')
 
-
 client = MongoClient("localhost:27017")
 db=client.gbgh
-db.velovs.create_index('timestamp', unique=True)
 
 now=datetime.datetime.now()
 timestamp = now.strftime("%s")
 
+####################################
+# insertion de la partie statique
+####################################
+
+if "staticVelov" not in db.collection_names():
+    db.staticVelov.create_index([("geometry", GEOSPHERE)])
+
+    r = requests.get("https://download.data.grandlyon.com/wfs/grandlyon"+
+        "?SERVICE=WFS"+
+        "&VERSION=2.0.0"+
+        "&outputformat=GEOJSON"+
+        "&maxfeatures=400"+
+        "&request=GetFeature"+
+        "&typename=pvo_patrimoine_voirie.pvostationvelov"+
+        "&SRSNAME=urn:ogc:def:crs:EPSG::4171")
+
+    logging.info("static velov http request status code is : %s" % r.status_code)
+
+    for station in r.json()["features"]:
+        toInsert = {}
+        toInsert["idstation"] = station["properties"]["idstation"]
+        toInsert["nom"] = station["properties"]["nom"]
+        toInsert["nbbornettes"] = station["properties"]["nbbornettes"]
+        toInsert["geometry"] = station["geometry"]
+
+        result = db.staticVelov.insert_one(toInsert)
+        logging.info("inserted staticVelovs with id %s" % result.inserted_id)
+
+####################################
+# insertion de la partie dynamique
+####################################
+
+if "dynamicVelov" not in db.collection_names():
+    db.dynamicVelov.create_index('timestamp', unique=True)
+
 r = requests.get("https://download.data.grandlyon.com/wfs/rdata" +
-   "?SERVICE=WFS&VERSION=2.0.0" +
+   "?SERVICE=WFS" +
+   "&VERSION=2.0.0" +
    "&outputformat=GEOJSON" +
-   "&maxfeatures=30" +
+   "&maxfeatures=400" +
    "&request=GetFeature" +
    "&typename=jcd_jcdecaux.jcdvelov" +
    "&SRSNAME=urn:ogc:def:crs:EPSG::4171")
 
-logging.info("http request status code is : %s" % r.status_code)
+logging.info("dynamic velov http request status code is : %s" % r.status_code)
 
-toInsert = {"timestamp" : timestamp, "geoJson" : r.json()}
-result = db.velovs.insert_one(toInsert)
+stationArray = []
+for feature in r.json()["features"]:
+    toInsertStation = {}
+    toInsertStation['idstation'] = feature['properties']['number']
+    toInsertStation['available_bikes'] = feature['properties']['available_bikes']
+    toInsertStation['available_bike_stands'] = feature['properties']['available_bike_stands']
+    stationArray.append(toInsertStation)
 
-logging.info("Inserted ID in database : %s" % result.inserted_id)
+toInsert = {'timestamp' : timestamp, "stations" : stationArray}
+result = db.dynamicVelov.insert_one(toInsert)
+
+logging.info("Inserted dynamic velovs with ID in database : %s" % result.inserted_id)
