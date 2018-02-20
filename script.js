88833c4905da848f6528dcaa4df76128cd344ce2
Louis Kraemer <louiskraemer@gmail.com>
2018-02-21 00:53:01 +0100

    Fetching json from api

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ d3.json("data/json/map.json", function (json) {
         duration = 750,
         velovDuration = 250,
         tooltipDuration = 250,
-        stationRadius = 10;
+        stationRadius = 8;
 
     d3.select("#map").attr('height', height);
 
@@ -40,8 +40,6 @@ d3.json("data/json/map.json", function (json) {
         });
 
     initVelov();
-
-    // resize();
 
     d3.select(window).on('resize', resize);
 
@@ -178,127 +176,13 @@ d3.json("data/json/map.json", function (json) {
         }, velovDuration);
     }
 
+    // d3.json('http://creti.fr/gbgh/endpoints/staticvelov.php', function (test) {
+    //     console.log(test)
+    // })
+
+    d3.json('http://creti.fr/gbgh/endpoints/staticvelov.php', function (test) {
+        console.log(test)
+    })
+
     // Slider config
-
-
-    //        width = parseInt(d3.select('#map').style('width')),
-    //        width = width - margin.left - margin.right,
-    //        mapRatio = .5,
-    //        height = width * mapRatio;
-
-    //    var height = 0,
-    //        width = 0,
-    //        center = d3.geoCentroid(json),
-    //        margin = {
-    //            top: '20px',
-    //            bottom: '20px',
-    //            left: '20px',
-    //            right: '20px'
-    //        },
-    //        svg = d3.select("body")
-    //        .append("svg")
-    //        .attr('id', 'map-container'),
-    //        projection = d3.geoConicConformal()
-    //        .translate([width / 2, height / 2])
-    //        .center(center)
-    //        .scale(200000),
-    //        path = d3.geoPath()
-    //        .projection(projection),
-    //        bounds = path.bounds(json);
-
-    //    const resize = function () {
-    //        console.log('resizing')
-    //        var scale = 100;
-    //        var height = body.clientHeight;
-    //        var width = body.clientWidth;
-    //        //        const hscale = scale * width / (bounds[1][0] - bounds[0][0]);
-    //        //        const vscale = scale * height / (bounds[1][1] - bounds[0][1]);
-    //        //        scale = (hscale < vscale) ? hscale : vscale;
-    //        //        const offset = [width - (bounds[0][0] + bounds[1][0]) / 2,
-    //        //                                height - (bounds[0][1] + bounds[1][1]) / 2];
-    //
-    //        var bounds = path.bounds(json);
-    //        var hscale = scale * width / (bounds[1][0] - bounds[0][0]);
-    //        var vscale = scale * height / (bounds[1][1] - bounds[0][1]);
-    //        var scale = (hscale < vscale) ? hscale : vscale;
-    //        var offset = [width - (bounds[0][0] + bounds[1][0]) / 2,
-    //                        height - (bounds[0][1] + bounds[1][1]) / 2];
-    //
-    //        console.log(scale)
-    //
-    //        projection = d3.geoConicConformal().center(center)
-    //            .scale(scale)
-    //        //            .translate(offset);
-    //        path = d3.geoPath()
-    //            .projection(projection)
-    //        //        projection.scale(scale).translate(offset)
-    //    }
-
-    //    resize();
-    //
-    //    window.onresize = resize;
-
-
-
-    //
-    //    projection.scale(scale).translate(offset);
-
-    //    
-
-    //    d3.csv("grippe.csv", function (data) {
-    //        d3.json("map.json", function (json) {
-    //            //On fusionne les donnees avec le GeoJSON des regions
-    //            var max = 0;
-    //            var values = []
-    //            for (var i = 0; i < data.length; i++) {
-    //                if (Number(data[i].somme2014) > max) {
-    //                    max = Number(data[i].somme2014);
-    //                }
-    //                values.push({
-    //                    nom: data[i].region,
-    //                    value: data[i].somme2014
-    //                })
-    //            }
-    //
-    //            var color = d3.scaleLinear().domain([1, 1000])
-    //                .interpolate(d3.interpolateHcl)
-    //                .range([d3.rgb("#FBE9E7"), d3.rgb('#BF360C')]);
-    //
-    //            svg.selectAll("path")
-    //                .style("fill", function (d) {
-    //                    for (var e of values) {
-    //                        if (e.nom === d.properties.nom) {
-    //                            return color(Math.floor(1000 * e.value / max))
-    //                        }
-    //                    }
-    //                })
-    //                .attr("grippe", function (f) {
-    //                    for (var e of values) {
-    //                        if (e.nom === f.properties.nom) {
-    //                            return e.value
-    //                        }
-    //                    }
-    //                })
-    //                .on('mousemove', function (e) {
-    //                    var mouse = d3.mouse(svg.node()).map(function (d) {
-    //                        return parseInt(d);
-    //                    });
-    //                    tooltip.classed('hidden', false)
-    //                        .attr('style', 'left:' + (mouse[0] + 15) +
-    //                            'px; top:' + (mouse[1] - 35) + 'px')
-    //                        .html(e.properties.nom);
-    //                })
-    //                .on('mouseout', function () {
-    //                    tooltip.classed('hidden', true);
-    //                })
-    //                .on('mouseleave', function (e) {
-    //                    d3.select(this).transition().style("stroke", "none")
-    //                })
-    //                .on('mouseenter', function (e) {
-    //                    d3.select(this).transition().style("stroke", "black")
-    //                })
-    //        });
-    //    });
-
-
 });
